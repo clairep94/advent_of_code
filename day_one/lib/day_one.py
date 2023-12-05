7a8f14a63147d99deb6a62edcb91b3c0398be4d8
ClairePeng <clairepeng94@gmail.com>
2023-12-05 13:02:43 +0000

    part one passed

--- day_one/lib/day_one.py
+++ day_one/lib/day_one.py
@@ -1,5 +1,6 @@
-def calibration_finder(value: str) -> str:
+def calibration_finder(value: str) -> int:
     digits = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"]
+
     value_digits = [char for char in value if char in digits]
 
     if len(value_digits) == 1:
@@ -7,4 +8,29 @@ def calibration_finder(value: str) -> str:
     else:
         return int(value_digits[0]+value_digits[-1])
     
+def calibration_finder_2(value:str) -> int:
+    digits = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"]
+    digits_spelled = {
+        "one": "1",
+        "two": "2",
+        "three": "3",
+        "four": "4",
+        "five": "5",
+        "six": "6",
+        "seven": "7",
+        "eight": "8",
+        "nine": "9"
+    }
+    
 
+    
+
+
+# def find_sum() -> int:
+#     with open ('Advent_of_Code/day_one/lib/calibration_codes.txt', 'r') as f:
+#         lines = [line for line in f]
+#         values_list = [calibration_finder(line) for line in lines]
+#         return sum(values_list)
+    
+
+# print(find_sum())
--- day_one/tests/test_day_one.py
+++ day_one/tests/test_day_one.py
@@ -3,7 +3,6 @@ from lib.day_one import *
 def test_1abc2():
     assert calibration_finder("1abc2") == 12
 
-
 def pqr3stu8vwx():
     assert calibration_finder("pqr3stu8vwx") == 38
 
@@ -12,3 +11,12 @@ def a1b2c3d4e5f():
 
 def treb7uchet():
     assert calibration_finder("treb7uchet") == 77
+
+def a9vxfg():
+    assert calibration_finder("9vxfg") == 99
+
+def cdtjprrbvkftgtwo397seven():
+    assert calibration_finder("cdtjprrbvkftgtwo397seven") == 37
+
+
+
